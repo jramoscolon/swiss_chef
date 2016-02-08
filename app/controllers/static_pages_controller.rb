b22c8d66ace2ec48fc6c314839e3ff16e29c2063
Jose Ramos <jramoscolon@yahoo.com>
2016-02-08 17:24:00 -0400

    Added the emails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,17 @@ class StaticPagesController < ApplicationController
   	@featured_product = Product.limit(3)
   end
 
+  def contact
+  end
+
+  def thank_you
+	  @name = params[:name]
+	  @email = params[:email]
+	  @message = params[:message]
+	  ActionMailer::Base.mail(:from => @email,
+	  	:to => 'jramoscolon@yahoo.com',
+	  	:subject => "A new contact form message from #{@name}",
+	  	:body => @message).deliver_now
+   end
+
 end
